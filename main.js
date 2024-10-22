d066368a9d70740b66ceda60f9d2f784d9924a09
Aduduluanku <gohanssj524@gmail.com>
2024-10-22 15:33:32 +0700

    thongtincanhan

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".test",{
+    strings: ["Web Developer" , "Joyful guy" , "VanLang University pupil"], 
+    typeSpeed: 100, 
+    backSpeed: 100, 
+    backDelay: 1000,
+    loop: true
+})
